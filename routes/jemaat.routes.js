141e77daf0555f6ac8561235f863f51bd748ec8a
gmmibaitesdanamosain-blip <gmmibaitesdanamosain@gmail.com>
2026-02-18 21:04:16 +0700

    Update jemaat.routes.js

--- routes/jemaat.routes.js
+++ routes/jemaat.routes.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import JemaatController from '../controllers/jemaat.controller.js';
 import { authenticateToken } from '../middlewares/auth.middleware.js';
-import { isAdmin } from '../middlewares/role.middleware.js';
+import { isSuperAdmin, isAdmin } from '../middlewares/role.middleware.js';
 
 const router = express.Router();
+
 router.get('/', JemaatController.getAll);
-router.get('/sectors', authenticateToken, JemaatController.getSectors);
 router.post('/', authenticateToken, isAdmin, JemaatController.create);
 router.put('/:id', authenticateToken, isAdmin, JemaatController.update);
 router.delete('/:id', authenticateToken, isAdmin, JemaatController.delete);
+router.get('/sectors', authenticateToken, JemaatController.getSectors);
 router.post('/sectors', authenticateToken, isAdmin, JemaatController.createSector);
 router.put('/sectors/:id', authenticateToken, isAdmin, JemaatController.updateSector);
 router.delete('/sectors/:id', authenticateToken, isAdmin, JemaatController.deleteSector);
