f8f24140f0e567c3ba975165473358061e9a09fe
gmmibaitesdanamosain-blip <gmmibaitesdanamosain@gmail.com>
2026-02-18 17:15:50 +0700

    Update renungan.repository.js

--- repositories/renungan.repository.js
+++ repositories/renungan.repository.js
@@ -1,42 +1,43 @@
-import prisma from '../config/prisma.js';
+import prisma from "../config/prisma.js";
 
 class RenunganRepository {
-    async findAll() {
-        return prisma.renungan_mingguan.findMany({
-            orderBy: { tanggal: 'desc' }
-        });
-    }
+  async findAll() {
+    return prisma.renungan_mingguan.findMany({
+      orderBy: { tanggal: "desc" },
+    });
+  }
 
-    async findById(id) {
-        return prisma.renungan_mingguan.findUnique({
-            where: { id: id }
-        });
-    }
+  async findById(id) {
+    return prisma.renungan_mingguan.findUnique({
+      where: { id },
+    });
+  }
 
-    async create(data) {
-        return prisma.renungan_mingguan.create({
-            data: {
-                ...data,
-                tanggal: data.tanggal ? new Date(data.tanggal) : new Date()
-            }
-        });
-    }
+  async create(data) {
+    return prisma.renungan_mingguan.create({
+      data: {
+        ...data,
+        tanggal: data.tanggal ? new Date(data.tanggal) : new Date(),
+      },
+    });
+  }
 
-    async update(id, data) {
-        return prisma.renungan_mingguan.update({
-            where: { id: id },
-            data: {
-                ...data,
-                tanggal: data.tanggal ? new Date(data.tanggal) : undefined
-            }
-        });
-    }
+  async update(id, data) {
+    return prisma.renungan_mingguan.update({
+      where: { id },
+      data: {
+        ...data,
+        tanggal: data.tanggal ? new Date(data.tanggal) : undefined,
+      },
+    });
+  }
 
-    async delete(id) {
-        return prisma.renungan_mingguan.delete({
-            where: { id: id }
-        });
-    }
+  async delete(id) {
+    return prisma.renungan_mingguan.delete({
+      where: { id },
+    });
+  }
 }
 
 export default new RenunganRepository();
+
