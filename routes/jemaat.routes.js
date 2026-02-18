3851319debfba174957a93a4c1df33990623792e
gmmibaitesdanamosain-blip <gmmibaitesdanamosain@gmail.com>
2026-02-18 21:17:16 +0700

    Update jemaat.routes.js

--- routes/jemaat.routes.js
+++ routes/jemaat.routes.js
@@ -1,18 +1,18 @@
 import express from 'express';
 import JemaatController from '../controllers/jemaat.controller.js';
 import { authenticateToken } from '../middlewares/auth.middleware.js';
-import { isSuperAdmin, isAdmin } from '../middlewares/role.middleware.js';
+import { isSuperAdmin } from '../middlewares/role.middleware.js';
 
 const router = express.Router();
 
 // Public endpoint for autocomplete (no authentication required)
 router.get('/', JemaatController.getAll);
-router.post('/', authenticateToken, isAdmin, JemaatController.create);
-router.put('/:id', authenticateToken, isAdmin, JemaatController.update);
-router.delete('/:id', authenticateToken, isAdmin, JemaatController.delete);
+router.post('/', authenticateToken, isSuperAdmin, JemaatController.create);
+router.put('/:id', authenticateToken, isSuperAdmin, JemaatController.update);
+router.delete('/:id', authenticateToken, isSuperAdmin, JemaatController.delete);
 router.get('/sectors', authenticateToken, JemaatController.getSectors);
-router.post('/sectors', authenticateToken, isAdmin, JemaatController.createSector);
-router.put('/sectors/:id', authenticateToken, isAdmin, JemaatController.updateSector);
-router.delete('/sectors/:id', authenticateToken, isAdmin, JemaatController.deleteSector);
+router.post('/sectors', authenticateToken, isSuperAdmin, JemaatController.createSector);
+router.put('/sectors/:id', authenticateToken, isSuperAdmin, JemaatController.updateSector);
+router.delete('/sectors/:id', authenticateToken, isSuperAdmin, JemaatController.deleteSector);
 
 export default router;
