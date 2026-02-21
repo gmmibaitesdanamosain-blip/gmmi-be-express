378e9792e4808443a26147877f51133b4e8e12bb
gmmibaitesdanamosain-blip <gmmibaitesdanamosain@gmail.com>
2026-02-21 11:25:41 +0700

    Update pewartaan.routes.js

--- routes/pewartaan.routes.js
+++ routes/pewartaan.routes.js
@@ -2,21 +2,17 @@ import express from 'express';
 import PewartaanController from '../controllers/pewartaan.controller.js';
 import { authenticateToken } from '../middlewares/auth.middleware.js';
 import { isSuperAdmin, isAdmin } from '../middlewares/role.middleware.js';
-
 const router = express.Router();
-
 // Public / Auth Member (can be read by all authenticated users if needed, or just admin)
 router.get('/', authenticateToken, PewartaanController.getAll);
 router.get('/:id', authenticateToken, PewartaanController.getById);
-
 // Export (Direct Link)
 router.get('/:id/export/excel', authenticateToken, PewartaanController.exportExcel);
 router.get('/:id/export/word', authenticateToken, PewartaanController.exportWord);
-
 // Admin and Super Admin
 router.post('/', authenticateToken, isAdmin, PewartaanController.create);
 router.put('/:id', authenticateToken, isAdmin, PewartaanController.update);
 router.delete('/:id', authenticateToken, isAdmin, PewartaanController.delete);
 router.patch('/:id/status', authenticateToken, isAdmin, PewartaanController.updateStatus);
-
 export default router;
+
