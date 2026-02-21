28fab4858b924784acf251e1bcd1763fa21d690d
gmmibaitesdanamosain-blip <gmmibaitesdanamosain@gmail.com>
2026-02-21 11:26:51 +0700

    Update pewartaan.service.js

--- services/pewartaan.service.js
+++ services/pewartaan.service.js
@@ -1,29 +1,24 @@
 import PewartaanRepository from '../repositories/pewartaan.repository.js';
-
 class PewartaanService {
     async getAll() {
         return await PewartaanRepository.findAll();
     }
-
     async getById(id) {
         return await PewartaanRepository.findById(id);
     }
-
     async create(data) {
         return await PewartaanRepository.create(data);
     }
-
     async update(id, data) {
         return await PewartaanRepository.update(id, data);
     }
-
     async delete(id) {
         return await PewartaanRepository.delete(id);
     }
-
     async updateStatus(id, status) {
         return await PewartaanRepository.updateStatus(id, status);
     }
 }
-
 export default new PewartaanService();
+
+
