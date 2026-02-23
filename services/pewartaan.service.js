f3684613abd7eb3d57ebefa9eb886f92352fd968
gmmibaitesdanamosain-blip <gmmibaitesdanamosain@gmail.com>
2026-02-23 10:38:51 +0700

    Update pewartaan.service.js

--- services/pewartaan.service.js
+++ services/pewartaan.service.js
@@ -1,24 +1,29 @@
 import PewartaanRepository from '../repositories/pewartaan.repository.js';
+
 class PewartaanService {
     async getAll() {
-        return await PewartaanRepository.findAll();
+        return PewartaanRepository.findAll();
     }
+
     async getById(id) {
-        return await PewartaanRepository.findById(id);
+        return PewartaanRepository.findById(id);
     }
+
     async create(data) {
-        return await PewartaanRepository.create(data);
+        return PewartaanRepository.create(data);
     }
+
     async update(id, data) {
-        return await PewartaanRepository.update(id, data);
+        return PewartaanRepository.update(id, data);
     }
-    async delete(id) {
-        return await PewartaanRepository.delete(id);
-    }
+
     async updateStatus(id, status) {
-        return await PewartaanRepository.updateStatus(id, status);
+        return PewartaanRepository.updateStatus(id, status);
+    }
+
+    async delete(id) {
+        return PewartaanRepository.delete(id);
     }
 }
+
 export default new PewartaanService();
-
-
