062e1f486af6f13a775316abe33e0fec8ea290bd
gmmibaitesdanamosain-blip <gmmibaitesdanamosain@gmail.com>
2026-02-23 16:05:16 +0700

    Update pewartaan.routes.js

--- routes/pewartaan.routes.js
+++ routes/pewartaan.routes.js
@@ -9,7 +9,7 @@ const router = express.Router();
 // Multer config - memory storage untuk Appwrite
 const upload = multer({
     storage: multer.memoryStorage(),
-    limits: { fileSize: 20 * 1024 * 1024 }, // 20MB max
+   limits: { fileSize: 4 * 1024 * 1024 }
     fileFilter: (req, file, cb) => {
         const allowedTypes = [
             'application/pdf',
