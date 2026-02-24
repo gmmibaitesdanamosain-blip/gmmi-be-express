400299a30c2b74d79407d0ec18fd3b5cbf16396f
gmmibaitesdanamosain-blip <gmmibaitesdanamosain@gmail.com>
2026-02-24 12:53:39 +0700

    Delete gmmi-be-express-main directory

--- gmmi-be-express-main/routes/arsip.routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import ArsipController from '../controllers/arsip.controller.js';
-
-const router = express.Router();
-
-router.get('/', ArsipController.getArsip);
-
-export default router;
